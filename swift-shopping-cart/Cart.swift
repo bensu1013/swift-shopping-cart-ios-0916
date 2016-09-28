15a0c5e3e6d7fc6dea9f23cb8823d07b590b3213
Ben Su <bensu1013@hotmail.com>
2016-09-28 15:43:54 -0400

    It's a miracle Jim. I did it.

--- swift-shopping-cart/Cart.swift
+++ swift-shopping-cart/Cart.swift
@@ -8,5 +8,76 @@
 
 class Cart {
     
+    var items: [Item] = []
     
+    func totalPriceInCents() -> Int {
+        var sum = 0
+        for item in items {
+            sum += item.priceInCents
+        }
+        return sum
+    }
+    
+    func add(item: Item) {
+        items.append(item)
+    }
+    
+    func remove(item: Item) {
+        for (index, i) in items.enumerated() {
+            if i == item {
+                items.remove(at: index)
+                break
+            }
+        }
+    }
+    
+    func items(withName name: String) -> [Item] {
+        var array: [Item] = []
+        for item in items {
+            if item.name == name {
+                array.append(item)
+            }
+        }
+        return array
+    }
+    
+    func items(withMinPrice price: Int) -> [Item] {
+        var array: [Item] = []
+        for item in items {
+            if item.priceInCents >= price {
+                array.append(item)
+            }
+        }
+        return array
+    }
+    
+    func items(withMaxPrice price: Int) -> [Item] {
+        var array: [Item] = []
+        for item in items {
+            if item.priceInCents <= price {
+                array.append(item)
+            }
+        }
+        return array
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
